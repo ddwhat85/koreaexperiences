05a13f996ffccd0fd60f5e5ab9b03f047fbb60d1
ddwhat85 <ddwhat85@gmail.com>
2026-06-23 19:29:06 +0900

    Refactor get_detail.php for contentId handling

---
+++ api/get_detail.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/config.php';
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=utf-8');
+$id=(int)($_GET['contentId']??0);
+if(!$id){echo '{}';exit;}
+$url='https://apis.data.go.kr/B551011/EngService2/detailCommon2'
+    .'?serviceKey='.urlencode(TOUR_API_KEY)
+    .'&MobileOS=ETC&MobileApp=KoreaExp&_type=json'
+    .'&contentId='.$id
+    .'&defaultYN=Y&overviewYN=Y';
+$r=@file_get_contents($url,false,stream_context_create(['http'=>['timeout'=>10]]));
+echo $r!==false?$r:'{}';
