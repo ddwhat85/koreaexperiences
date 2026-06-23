fb19137841a6175d345136b3ba3ba54ff4b03f7a
ddwhat85 <ddwhat85@gmail.com>
2026-06-23 16:25:17 +0900

    Delete api/setup.php

--- api/setup.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!empty($_POST['k'])){
-    $f=__DIR__.'/config.php';
-    if(!file_exists($f)){
-          file_put_contents($f,'<?php define("TOUR_API_KEY","'.$_POST['k'].'"); ?>');
-          echo 'created';
-    }else{echo 'exists';}
-}else{echo 'POST k=KEY';}
