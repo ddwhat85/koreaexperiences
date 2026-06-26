08db29349af9c0b83ab61c85ae8ffd2ea6daa033
ddwhat85 <ddwhat85@gmail.com>
2026-06-26 18:32:25 +0900

    Serve sitemap.xml for specific request

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+if ($_SERVER['REQUEST_URI'] === '/sitemap.xml') {
+  header('Content-Type: application/xml; charset=UTF-8');
+  readfile(__DIR__ . '/sitemap.xml');
+  exit;
+}
 header('Location: /index.html', true, 301);
 exit;
 ?>
