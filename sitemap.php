2bb6e77dbb8c9730f6e9d7160da258142311d941
ddwhat85 <ddwhat85@gmail.com>
2026-06-26 20:41:36 +0900

    Create sitemap.php for XML sitemap generation

---
+++ sitemap.php
@@ -0,0 +1,12 @@
+<?php
+header('Content-Type: application/xml; charset=UTF-8');
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+echo '<url>';
+echo '<loc>https://koreaexperiences.com/</loc>';
+echo '<lastmod>2026-06-26</lastmod>';
+echo '<changefreq>weekly</changefreq>';
+echo '<priority>1.0</priority>';
+echo '</url>';
+echo '</urlset>';
+?>
